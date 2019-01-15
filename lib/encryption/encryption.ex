b95bbcd897e7d807231abeaab42fe0199ee3b9a2
silbermm <silbermm@gmail.com>
2019-01-15 14:02:16 -0500

    airport coding

---
+++ lib/encryption/encryption.ex
@@ -0,0 +1,7 @@
+defmodule Genex.Encryption do
+  @moduledoc """
+    Defines how to load and encrypted file and save plain text to encrypted file
+  """
+  @callback load :: {:ok, term} | {:error, String.t}
+  @callback save(String.t) :: term
+end
--- lib/encryption/gpg.ex
+++ lib/encryption/gpg.ex
@@ -1,15 +1,19 @@
-defmodule Genex.GPG do
+defmodule Genex.Encryption.GPG do
   @moduledoc """
   Deals with all aspects of GnuPG
   """
+  alias Genex.Encryption
+ 
+  @behavior @Encryption
 
   @doc """
-  Load a gpg encrypted file into memory
+  Load the genex gpg encrypted file into memory. If the file doesn't exist, returns an error.
   """
+  @impl Encryption
   def load do
     file = System.get_env("HOME") <> "/" <> ".genex_passwords.gpg"
 
-    with {result, 0} <- System.cmd("gpg", ["--decrypt", "--quiet", "--no-tty", "--batch", file]) do
+    with {result, 0} <- System.cmd("gpg", ["--decrypt", "--quiet", "--no-tty", "--batch", file, " 2>&1"]) do
       {:ok, result}
     else
       {error, code} ->
@@ -21,8 +25,9 @@ defmodule Genex.GPG do
   end
 
   @doc """
-  Save data to a file and encrypt it with GPG
+  Save data to the genex password file and encrypt it with GPG.
   """
+  @impl Encryption
   def save(data) do
     filename = System.get_env("HOME") <> "/" <> ".genex_passwords.gpg"
 
@@ -31,5 +36,12 @@ defmodule Genex.GPG do
     }"
     |> String.to_char_list()
     |> :os.cmd()
+  end
+
+  @doc """
+  Generate a GPG public/private key combination to use when encyrpting/decrypting the genex passwords file.
+  """
+  def generate_keys do
+
   end
 end
