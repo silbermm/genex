85bc5b08e13ad51c7410e6e43ba35ee6e6101b22
Matt Silbernagel <silbermm@gmail.com>
2020-09-19 14:53:33 -0400

    new files

---
+++ lib/genex/password_file.ex
@@ -0,0 +1,23 @@
+defmodule Genex.PasswordFile do
+  @moduledoc """
+  Takes care of safely loading and writing the passwords file.
+  """
+
+  alias Genex.Environment
+
+  @spec load() :: {:ok, list()} | :error
+  def load() do
+    with filename <- Environment.load_variable("GENEX_PASSWORDS", :passwords_file),
+         {:ok, file} <- File.read(filename) do
+      Jason.decode(file)
+    else
+      _err -> :error
+    end
+  end
+
+  @spec write(binary()) :: :ok | {:error, atom()}
+  def write(data) do
+    filename = Environment.load_variable("GENEX_PASSWORDS", :passwords_file)
+    File.write(filename, data)
+  end
+end
---
+++ lib/genex/random_number.ex
@@ -0,0 +1,13 @@
+defmodule Genex.RandomNumber do
+  @doc "Generate a randomn number string from 11111 to 66666"
+  @spec random_number :: binary()
+  def random_number() do
+    1..5
+    |> Enum.map(fn _ -> Task.async(&single_random/0) end)
+    |> Task.yield_many()
+    |> Enum.map(fn {_task, {:ok, num}} -> num end)
+    |> Enum.join()
+  end
+
+  defp single_random, do: Enum.random(1..6)
+end
