0230003b6526e331b41a0f448ba380006b4ff8f7
Matt Silbernagel <silbermm@gmail.com>
2021-04-18 20:15:41 -0400

    fix runtime config

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,4 +1,6 @@
 import Config
 
-config :genex,
-  genex_home: System.get_env("GENEX_HOME") || Path.join(System.get_env("HOME"), ".genex")
+if config_env() != :test do
+  config :genex,
+    genex_home: System.get_env("GENEX_HOME") || Path.join(System.get_env("HOME"), ".genex")
+end
