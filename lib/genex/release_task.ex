ab4edcf18fa97e65a32ab3aab6e619e17a49e990
Matt Silbernagel <matt@silbernagel.dev>
2022-06-27 22:45:11 -0400

    Remove unused main/0 fn

--- lib/genex/release_task.ex
+++ lib/genex/release_task.ex
@@ -1,14 +1,5 @@
 defmodule Genex.ReleaseTask do
-  def run() do
-    # Eval commands needs to start the app before
-    # Or Application.load(:my_app) if you can't start it
-    Application.ensure_all_started(:genex)
-    Genex.CLI.main()
-  end
-
   def run(args) do
-    # Eval commands needs to start the app before
-    # Or Application.load(:my_app) if you can't start it
     Application.ensure_all_started(:genex)
     Genex.CLI.main(args)
   end
