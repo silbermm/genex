d04401308f7dcc94705a4f6df3731b239dfaf723
Matt <matt@silbernagel.dev>
2023-08-27 19:13:36 -0400

    Runtime config for Logger file

--- config/runtime.exs
+++ config/runtime.exs
@@ -7,3 +7,16 @@ config :genex,
 
 config :mnesia,
   dir: base_path |> Path.join("genex") |> Path.join("db") |> String.to_charlist()
+
+config :logger, :default_handler,
+  config: [
+    file: ~c"#{base_path}/genex/genex.log",
+    filesync_repeat_interval: 5000,
+    file_check: 5000,
+    max_no_bytes: 10_000_000,
+    max_no_files: 5,
+    compress_on_rotate: true
+  ],
+  compile_time_purge_matching: [
+    [level_lower_than: :info]
+  ]
