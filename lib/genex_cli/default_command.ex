a302e6d689d5da46bb64db93e19dfca1205cac91
Matt <matt@silbernagel.dev>
2023-12-23 16:40:13 -0500

    add GPL

--- lib/genex_cli/default_command.ex
+++ lib/genex_cli/default_command.ex
@@ -1,15 +1,6 @@
 defmodule GenexCLI.DefaultCommand do
   @moduledoc """
-
-  By default gets all the saved passwords from 
-  the data store and displays them (passphrases hidden)
-  on the screen.
-
-  OPTIONS
-  -------
-    --profile       which profile to use
-
-    --help          show this help
+  #{Module.get_attribute(GenexCLI, :moduledoc)}
   """
   use Prompt.Command
 
